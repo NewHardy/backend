4f55a7e76616c245c000816d8ef81e80ff016aa7
Andrey Gavrilov <NewHardyMan09@gmail.com>
2025-07-20 17:20:01 +0200

    initial commit

---
+++ FirstSpringBoot/src/main/java/com/example/FirstSpringBoot/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.FirstSpringBoot.controller;
+
+import com.example.FirstSpringBoot.entity.User;
+import com.example.FirstSpringBoot.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class UserController {
+    private final UserRepository repository;
+
+    public UserController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public User save(@RequestBody User user) {
+        return repository.save(user);
+    }
+}
---
+++ FirstSpringBoot/src/main/java/com/example/FirstSpringBoot/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.FirstSpringBoot.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "Pokemons")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String image;
+    private String name;
+    private ArrayList<String> types;
+    private String evoFrom;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public ArrayList<String> getTypes() { return types; }
+    public void setTypes(ArrayList<String> types) { this.types = types; }
+
+    public String getEvoFrom() { return evoFrom; }
+    public void setEvoFrom(String evoFrom) { this.evoFrom = evoFrom; }
+}
